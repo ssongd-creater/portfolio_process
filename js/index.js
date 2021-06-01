772d0f7a280961bdf5fb7371421429851062e5c9
ssongd-creator <https://github.com/ssongd-creater/test_0524.git>
2021-06-01 15:39:44 +0900

    06/01 schedule css/js 반응형 update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,14 @@
+//Cutting Contents Text
 const conTxt = document.querySelectorAll('.con-txt p a');
 conTxt.forEach(element => {
   //console.log(element.textContent.slice(0,10) + "...");
   const cutTxt = element.textContent.slice(0, 10) + "...";
   element.textContent = cutTxt;
 });
+
+//Mobile Menu Activate
+const mobileMenu = document.querySelector('.mobile-menu');
+
+mobileMenu.onclick = () => {
+  mobileMenu.classList.toggle('active');
+}
