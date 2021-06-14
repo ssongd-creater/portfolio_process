e8dd123dd7e6b47c6305a97e7843e5efa94821fb
ssongd-creator <https://github.com/ssongd-creater/test_0524.git>
2021-06-14 18:33:02 +0900

    06/14 AJAX > JAVASCRIPT로 변경

--- php/sp_insert.php
+++ php/sp_insert.php
@@ -18,6 +18,26 @@
   
   mysqli_query($dbConn, $sql);
 
+  //--------------
+  $sql1 = "SELECT * FROM sp_table ORDER BY SP_idx DESC";
+
+  $sp_result = mysqli_query($dbConn, $sql1);
+
+  $arr = array();
+
+  while($sp_row = mysqli_fetch_array($sp_result)){
+    array_push($arr, array(
+      'sp_idx' => $sp_row['SP_idx'],
+      'sp_cate' => $sp_row['SP_cate'],
+      'sp_tit' => $sp_row['SP_tit'],
+      'sp_con' => $sp_row['SP_con'],
+      'sp_reg' => $sp_row['SP_reg']
+    ));
+  }
+
+  file_put_contents($_SERVER["DOCUMENT_ROOT"].'/schedule/data/sp_table.json',json_encode($arr,JSON_UNESCAPED_UNICODE));
+  
+  //************ 
   echo"
   <script>
     alert('작성이 완료되었습니다');
